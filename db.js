be6de7ec78adca559232e6a2edf987ac0dfd90f8
Mattia Alfieri <cikkense@gmail.com>
2018-01-01 19:48:02 +0100

    Fixed logs view and added persitance options to db class

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs')
 const EventEmitter = require('events');
 
@@ -6,31 +5,57 @@ class Jar {
 
   constructor() {
     this.emitter = new EventEmitter()
+    this.configuration = {
+      filepath : './jar.json',
+      /**
+       * When should the data be persisted to the filepath
+       * Possible values are
+       * set: Save to disk at each set() call
+       * interval: Save to disk every N ms
+       * never: Never persist to disk
+       * @type {String}
+       */
+      peristanceStrategy : 'set'
+    }
     this.data = {}
-    fs.readFile('./jar.json','utf-8', (err, file) => {
+    fs.readFile(this.configuration.filepath, 'utf-8', (err, file) => {
       if (err) {
-        console.log("No file available. Creating a new one");
-         this.persist(()=>{
-            console.log("New db file created")
-          })
+        console.log("No database file available. Creating a new one...");
+        this.persist(() => {
+          console.log("New database file created.")
+        })
       } else {
         try {
           this.data = JSON.parse(file)
         } catch (e) {
-          this.emitter.emit('error',e)
-          console.log("jar.json file is not valid json. Creating a new one.")
-          this.persist(()=>{
+          this.emitter.emit('error', e)
+          console.log(this.configuration.filepath+" file is not valid json. Creating a new one.")
+          this.persist(() => {
             console.log("New db file created")
           })
         }
       }
+
+      /**
+       * If the persistance strategy is interval then we register the interval
+       */
+      if (this.configuration.peristanceStrategy === 'interval') {
+        setInterval(() => {
+          this.persist()
+        })
+      }
     })
   }
 
+  /**
+   * Writes current data to a file and calls optional cllback with error
+   * @param  {Function} cb [description]
+   */
   persist(cb) {
-    fs.writeFile('./jar.json',JSON.stringify(this.data), (err) => {
+    let json = JSON.stringify(this.data)
+    fs.writeFile(this.configuration.filepath, json, (err) => {
       if (err) {
-        this.emitter.emit('error',err)
+        this.emitter.emit('error', err)
       } else {
         this.emitter.emit('persisted')
         if ("function" === typeof cb) {
@@ -45,9 +70,11 @@ class Jar {
     return this.data[key]
   }
 
-  set(key,value) {
+  set(key, value) {
     this.data[key] = value
-    return this.persist()
+    if (this.configuration.peristanceStrategy === 'interval') {
+      this.persist()
+    }
   }
 }
 
